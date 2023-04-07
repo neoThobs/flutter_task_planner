651c8c456883e4fa6fd9755b585698d032078edb
Oupa Bae <129596411+neoThobs@users.noreply.github.com>
2023-04-07 20:51:15 +0200

    feat: added pages for screens

---
+++ lib/screens/create_new_task_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateNewTaskPage extends StatelessWidget {
+  const CreateNewTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
